3b3b0e4660502b07bf07ed4be7dbc3b072251661
Radostina <radi.chipanova@gmail.com>
2021-04-14 14:41:20 +0300

    Some changes.

--- test.cpp
+++ test.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main() {
     // 1st of January
-   int headsToday = 3;
+   int headsToday;
    int Sandwiches2DaysAgo = 1;
    int Sandwiches1DayAgo = 2;
    int daysFromNewYear = 1;
    int n;
    cin >> n;
-   while(daysFromNewYear != n){
+   while(daysFromNewYear != n  ){
 
      Sandwiches2DaysAgo = Sandwiches1DayAgo;
      Sandwiches1DayAgo = headsToday - daysFromNewYear;
